d40cd51d3e878d4f64a4a8f9d0f29b69c481caba
Jitlee <www.wpj@163.com>
2016-07-13 18:42:51 +0800

    提交项目

---
+++ code/server/apps/admin/config.php
@@ -0,0 +1,8 @@
+<?php
+//配置文件
+return [
+	'view_replace_str'	=> [	// 模版渲染输出
+		'__JS__'		=> '/static/admin/js',
+		'__LIBS__'		=> '/static/libs'
+	]
+];
---
+++ code/server/runtime/temp/1fee89043be25ad5b64eff26a3199b3c.php
@@ -0,0 +1,19 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:68:"E:\Works\thinkphp-swagger\public/../apps/admin\view\index\index.html";i:1468400435;}*/ ?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+		<title></title>
+	</head>
+	<body ng-app="admin">
+		<div ui-view=""></div>
+		<script src="__LIBS__/angular/angular.js"></script>
+		<script src="__LIBS__/angular-ui-router/angular-ui-router.min.js"></script>
+		<script src="__LIBS__/ocLazyLoad/ocLazyLoad.min.js"></script>
+		<script src="__LIBS__/angular-ui-router-require-polyfill/ui-router-require-polyfill.js"></script>
+		
+		<script src="__JS__/app.js"></script>
+		<script src="__JS__/router.js"></script>
+	</body>
+</html>
